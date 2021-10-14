71a21dd97269edb16b293b3abbf115a0d0d3bda6
michalkedzia <222463@edu.p.lodz.pl>
2021-10-14 13:08:13 +0200

    Changing request maping to get, add api path

--- src/main/java/com/mk/rev/ReverseAPI.java
+++ src/main/java/com/mk/rev/ReverseAPI.java
@@ -1,13 +1,15 @@
 package com.mk.rev;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/rev")
 public class ReverseAPI {
 
-    @RequestMapping(path = "/{stringToReverse}/")
+    @GetMapping("/{stringToReverse}/")
     public String reverseString(@PathVariable("stringToReverse") String stringToReverse) {
         StringBuilder stringBuilder = new StringBuilder(stringToReverse);
         return stringBuilder.reverse().toString();
