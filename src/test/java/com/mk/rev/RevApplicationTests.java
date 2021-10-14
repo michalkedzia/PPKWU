550b3c3c0457090a0a2699ef8b36799e66417b1b
michalkedzia <222463@edu.p.lodz.pl>
2021-10-14 13:25:21 +0200

    Adding unit test

--- src/test/java/com/mk/rev/RevApplicationTests.java
+++ src/test/java/com/mk/rev/RevApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mk.rev;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ReverseAPI.class)
 class RevApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void shouldReturnReverseString() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/rev/123/");
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        Assertions.assertEquals("321", mvcResult.getResponse().getContentAsString());
     }
 
 }
