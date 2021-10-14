b948ffc9383dd4c2072d5fd0cf9c4f66e1dcafd5
michalkedzia <222463@edu.p.lodz.pl>
2021-10-14 13:00:26 +0200

    Adding a class with the rest method

---
+++ src/main/java/com/mk/rev/ReverseAPI.java
@@ -0,0 +1,14 @@
+package com.mk.rev;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReverseAPI {
+
+    @RequestMapping(path = "/{stringToReverse}/")
+    public String reverseString(@PathVariable("stringToReverse") String stringToReverse) {
+        return stringToReverse;
+    }
+}
